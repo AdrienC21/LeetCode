7682bf2db027adfc5bf2f26f7bb4eb1bc4e939c0
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-04-27 19:14:39 +0200

    Time: 56 ms (8.13%), Space: 13.9 MB (73.50%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        c = 1
+        for k in range((n+1)//2):
+            for j in range(k, n-1-k):
+                matrix[k][j] = c
+                c += 1
+            for i in range(k, n-1-k):
+                matrix[i][n-1-k] = c
+                c += 1
+            for j in range(n-1-k, k, -1):
+                matrix[n-1-k][j] = c
+                c += 1
+            for i in range(n-1-k, k, -1):
+                matrix[i][k] = c
+                c += 1
+        if n % 2 == 1:  # fill the value at the middle
+            matrix[n//2][n//2] = c
+        return matrix
