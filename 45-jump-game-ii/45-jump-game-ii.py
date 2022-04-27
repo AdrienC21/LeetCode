17131a2dbc2d6e32a70f4d5635f6bbcf46513187
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-04-27 19:12:43 +0200

    Time: 17780 ms (5.01%), Space: 27.7 MB (5.00%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [-1 for _ in range(n)]
+        dp[-1] = 0
+        def recSearch(i: int) -> int:
+            nonlocal dp, n
+            
+            if dp[i] == -1:  # never calculated
+                maxJump = nums[i]
+                res = n + 1
+                for j in range(1, maxJump+1):
+                    if (i + j) < n:
+                        res = min(res, 1 + recSearch(i+j))
+                dp[i] = res
+            return dp[i]
+
+        return recSearch(0)
