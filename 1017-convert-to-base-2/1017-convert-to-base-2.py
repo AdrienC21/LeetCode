75897ae4dfd790b138671a5b7f0dd98a1d47efff
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-04-27 19:29:52 +0200

    Time: 36 ms (74.26%), Space: 13.9 MB (10.89%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def baseNeg2(self, n: int) -> str:
+        if n == 0:
+            return "0"
+        if n == 1:
+            return "1"
+        if (n % 2) == 1:
+            return self.baseNeg2(-((n-1)//2)) + "1"
+        return self.baseNeg2(-(n//2)) + "0"
