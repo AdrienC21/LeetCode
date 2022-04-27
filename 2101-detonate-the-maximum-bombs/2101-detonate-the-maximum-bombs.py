452ea152e59e0c3aacd0b582e4d5fa7e9f1084a2
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-04-27 20:13:19 +0200

    Time: 1892 ms (24.57%), Space: 14.3 MB (61.44%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -0,0 +1,29 @@
+class Solution:
+    def propagate(self, b1: List[int], b2: List[int]) -> Tuple[bool]:
+        d = ((b1[0] - b2[0])**2 + (b1[1] - b2[1])**2)
+        return  (d <= b1[2]**2, d <= b2[2]**2)
+        
+    def dfs(self, G: dict, i: int, visited: set):
+        if i in visited:
+            return
+        visited.add(i)
+        for j in G[i]:
+            self.dfs(G, j, visited)
+
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        n = len(bombs)
+        G = {i: [] for i in range(n)}
+        for i in range(n-1):
+            bi = bombs[i]
+            for j in range(i+1, n):
+                c1, c2 = self.propagate(bi, bombs[j])
+                if c1:  # bj in radius of bi, so bi propagate to bj
+                    G[i].append(j)
+                if c2:
+                    G[j].append(i)
+        maxbombs = 1
+        for b in G:
+            visited = set()
+            self.dfs(G, b, visited)
+            maxbombs = max(maxbombs, len(visited))
+        return maxbombs
