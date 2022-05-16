9f1a7968c88a42f5b5e58391345d5252561cb489
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-05-16 15:01:02 +0200

    Time: 38 ms (64.26%), Space: 13.9 MB (10.83%) - LeetHub

---
+++ 504-base-7/504-base-7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def recConvert(self, num: int) -> List[str]:
+        if num < 0:
+            return self.recConvert(-num)
+        if num < 7:
+            return [str(num)]
+        return self.recConvert(num // 7) + [str(num % 7)]
+        
+    def convertToBase7(self, num: int) -> str:
+        L = self.recConvert(abs(num))
+        res = "".join(L)
+        if num < 0:
+            return str(-int(res))
+        return res
