75740ba7107dc35df7fcd30e5d2449d05b4f0226
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-05-16 12:48:28 +0200

    Time: 2 ms (82.29%), Space: 5.5 MB (48.00%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.c
@@ -0,0 +1,11 @@
+
+
+int arrangeCoins(int n){
+    double delta = n;
+    delta = delta * 8;
+    delta = delta + 1;
+    double res = (-1 + sqrt(delta)) / 2;
+    int final_res;
+    final_res = res;
+    return final_res;
+}
