e456942f13530dd3bea8a2067845b6d52ceb163f
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-05-16 12:10:51 +0200

    Time: 3342 ms (5.00%), Space: 26 MB (5.90%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+import sys
+import heapq
+class Solution:
+    def getNeighbors(self, i: int, j: int, n: int, grid: List[List[int]]):
+        res = []
+        for k in (-1, 0, 1):
+            for l in (-1, 0, 1):
+                if not((k == 0) and (l == 0)) and (0 <= (i+k) < n) and (0 <= (j+l) < n) and (grid[i+k][j+l] == 0):
+                    res.append((1, (i+k, j+l)))
+        return res
+        
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0] != 0:
+            return -1
+        # build a graph
+        n = len(grid)
+        visited = set()
+        to_visit = [(0, 0)]
+        graph = defaultdict(list)
+        while to_visit:
+            i, j = to_visit.pop()
+            if (i, j) in visited:
+                continue
+            visited.add((i, j))
+            for d, (k, l) in self.getNeighbors(i, j, n, grid):
+                graph[(i, j)].append((d, (k, l)))
+                if (k, l) not in visited:
+                    to_visit.append((k, l))
+        
+        if (n-1, n-1) not in visited:  # we can't reach it
+            return -1
+        # else, seek for the shortest path using djikstra among all the reachable path
+        distances = {(i, j): sys.maxsize for (i, j) in visited}
+        distances[(n-1, n-1)] = 0
+        visited = set()
+        to_visit = []
+        heapq.heappush(to_visit, (0, (n-1, n-1)))
+        while to_visit:
+            _, (i, j) = heapq.heappop(to_visit)
+            if (i, j) in visited:
+                continue
+            visited.add((i, j))
+            for d, (k, l) in graph[(i, j)]:
+                if ((k, l) not in visited) and (distances[(k, l)] > distances[(i, j)] + d):
+                    distances[(k, l)] = distances[(i, j)] + d
+                    heapq.heappush(to_visit, (distances[(k, l)], (k, l)))
+        return distances[(0, 0)] + 1  # length of a clear path is the number of visited cells of this path.
