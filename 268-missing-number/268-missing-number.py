ccd721959a7c6c6810911153e5990377fd45910c
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-05-28 16:01:44 +0200

    Time: 183 ms (53.61%), Space: 15.2 MB (34.52%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,7 +1,15 @@
 class Solution:
+    # O(1) space, O(n) time
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        size = (n * (n + 1)) // 2
+        return int(size - sum(nums))
+    # bad, nlog n
+    """
     def missingNumber(self, nums: List[int]) -> int:
         nums.sort()
         for i, n in enumerate(nums):
             if i != n:
                 return i
         return n+1
+    """
