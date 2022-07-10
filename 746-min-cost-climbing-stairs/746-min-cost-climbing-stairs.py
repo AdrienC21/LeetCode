53252497bfb486fdf38228633ab1477b13182020
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-07-10 16:23:10 +0200

    Time: 78 ms (68.12%), Space: 14 MB (75.93%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -2,7 +2,7 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         cost.append(0)  # add the top floor
         n = len(cost)
-        d = deque([n-1])
+        d = deque([n-1])  # for every i, store the reacheable position (here within two steps) with the lowest cumulative cost
         for i in range(n-2, -1, -1):
             if (d[0] - i) > 2:  # max two steps
                 d.popleft()
