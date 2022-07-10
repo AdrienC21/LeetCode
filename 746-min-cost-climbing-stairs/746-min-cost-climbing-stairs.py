78ccc4a012f2ed4c1774ce1dd7f4e78d50d837b0
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-07-10 16:21:04 +0200

    Time: 78 ms (68.12%), Space: 14 MB (44.40%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)  # add the top floor
+        n = len(cost)
+        d = deque([n-1])
+        for i in range(n-2, -1, -1):
+            if (d[0] - i) > 2:  # max two steps
+                d.popleft()
+            cost[i] += cost[d[0]]
+            while d and cost[d[-1]] >= cost[i]:
+                d.pop()
+            d.append(i)
+        return min(cost[0], cost[1])
