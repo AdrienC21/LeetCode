3d92d8bcd2c3d21eb478c0cceb14af3e2d39e349
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2022-10-08 16:21:06 +0200

    Time: 220 ms (97.11%), Space: 16.3 MB (84.20%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,22 +1,39 @@
-class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        closest = sum(nums[:3])
-        n = len(nums)
-        for i in range(n-2):
-            l = i + 1
-            r = n - 1
-            nbi = nums[i]
-            while l < r:
-                s = nbi + nums[l] + nums[r]
-                if s == target:
-                    return target
-                elif s < target:
-                    l += 1
-                    if abs(target - s) < abs(target - closest):
-                        closest = s
-                else:
-                    r -= 1
-                    if abs(target - s) < abs(target - closest):
-                        closest = s
-        return closest
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int closest = nums[0] + nums[1] + nums[2];
+        int n = nums.size();
+        
+        int l;
+        int r;
+        int s;
+        int nbi;
+        for (int i=0; i<(n-2); i++) {
+            l = i + 1;
+            r = n - 1;
+            nbi = nums[i];
+            while (l < r) {
+                s = nbi + nums[l] + nums[r];
+                if (s == target) {
+                    return target;
+                }
+                else {
+                    if (s < target) {
+                        l++;
+                        if (std::abs(target - s) < std::abs(target - closest)) {
+                            closest = s;
+                        }
+                    }
+                    else {
+                        r--;
+                        if (std::abs(target - s) < std::abs(target - closest)) {
+                            closest = s;
+                        }
+                    }
+                }
+            }
+        }
+        return closest;
+    }
+};
