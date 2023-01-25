3b59d986c5afc6e776cef86c936acc144c0594cf
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2023-01-25 01:13:27 +0100

    Time: 201 ms (39.42%), Space: 14 MB (67.63%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,26 +1,38 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
-        ans = 0
-        q = collections.deque([1])
+        res = 0
+        d = deque()  # positions to explore
+        d.append(1)
         seen = set()
-        A = [0] * (1 + n * n)  # 2D -> 1D
-
+        n2 = n**2  # to avoid computing it all the time
+        
+        # fill a 1D versio of the board
+        board1D = (n2 + 1) * [0]
         for i in range(n):
-            for j in range(n):
-                A[(n - 1 - i) * n + (j + 1 if n - i & 1 else n - j)] = board[i][j]
+            base_pos = n * (n - 1 - i)
+            if (n - i) % 2 == 1:  # odd line
+                base_pos += 1
+                for j in range(n):
+                    board1D[base_pos + j] = board[i][j]
+            else:
+                base_pos += n
+                for j in range(n):
+                    board1D[base_pos - j] = board[i][j]
 
-        while q:
-            ans += 1
-            for _ in range(len(q)):
-                curr = q.popleft()
-                for next in range(curr + 1, min(curr + 6, n * n) + 1):
-                    dest = A[next] if A[next] > 0 else next
-                    if dest == n * n:
-                        return ans
-                    if dest in seen:
-                        continue
-                    q.append(dest)
-                    seen.add(dest)
+        while d:  # explore positions
+            res += 1
+            for _ in range(len(d)):
+                curr = d.popleft()
+                for next_pos in range(curr + 1, min(curr + 6, n2) + 1):
+                    if board1D[next_pos] > 0:  # ladder or snake
+                        destination = board1D[next_pos]  # take it
+                    else:
+                        destination = next_pos
+                    if destination == n2:
+                        return res
+                    if destination not in seen:
+                        d.append(destination)
+                        seen.add(destination)
 
-        return -1
+        return -1  # no path to the last cell
