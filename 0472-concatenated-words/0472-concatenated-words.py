d735159b0dbb0d6a28f8e8e66ee2b66f6822f5b6
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2023-01-27 13:46:17 +0100

    Time: 401 ms (73.53%), Space: 27.7 MB (47.98%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -0,0 +1,22 @@
+class Solution:
+    # set for speed up
+    # dict to cache previous results
+    
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        allwords = set(words)
+        calculated = {}
+
+        def is_concatenated(w: str) -> bool:
+            nonlocal allwords, calculated
+            
+            if w in calculated:
+                return calculated[w]
+
+            for i in range(1, len(w)):
+                begin = w[:i]
+                end = w[i:]
+                if (begin in allwords) and ((end in allwords) or is_concatenated(end)):
+                    return True
+            return False
+
+        return [w for w in allwords if is_concatenated(w)]
