3dad6536ca4ea0258171dd72ac5c9324a2970e62
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2023-03-20 00:16:34 +0000

    Time: 167 ms (65.85%), Space: 14.4 MB (65.78%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -9,7 +9,7 @@ class Solution:
             if n < 1:
                 break
             if flowerbed[i] == 0:
-                if ((i == 0) and (flowerbed[1] == 0)) or ((i == (flowers - 1)) and (flowers >= 2) and (flowerbed[flowers-2] == 0)) or ((flowerbed[i-1] == 0) and (flowerbed[i+1] == 0)):
+                if ((i == 0) and (flowerbed[1] == 0)) or ((i == (flowers - 1)) and (flowerbed[flowers-2] == 0)) or ((flowerbed[i-1] == 0) and (flowerbed[i+1] == 0)):
                     flowerbed[i] = 1
                     n -= 1
         return (n < 1)
