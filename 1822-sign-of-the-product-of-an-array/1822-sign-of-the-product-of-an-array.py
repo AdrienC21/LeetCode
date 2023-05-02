90cecdda01d5bf28d7ba63d9354f99910db05de5
Adrien Carrel <21308422+AdrienC21@users.noreply.github.com>
2023-05-02 10:09:48 +0100

    Time: 69 ms (16.83%), Space: 16.4 MB (7.53%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,5 +1,20 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
+        nb_neg = 0
+        for n in nums:
+            if not(n):
+                return 0
+            if n < 0:
+                if nb_neg:
+                    nb_neg = 0
+                else:
+                    nb_neg = 1
+        if nb_neg:
+            return -1
+        return 1
+
+    """
+    def arraySign(self, nums: List[int]) -> int:
         res = 1
         for n in nums:
             if not(n):
@@ -7,3 +22,4 @@ class Solution:
             if n < 0:
                 res *= -1
         return res
+    """
